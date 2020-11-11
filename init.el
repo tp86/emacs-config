43ea8b65961d5bdf1311ae3e45fb944645eaf17b
tp86 <tom0palka+tp86@gmail.com>
2020-11-11 22:02:46 +0100

    Basic UI configuration

---
+++ init.el
@@ -0,0 +1,15 @@
+(setq inhibit-startup-message t)
+
+(if (display-graphic-p)
+    ((scroll-bar-mode -1)
+     (tool-bar-mode -1)
+     (tooltip-mode -1)
+     (set-fringe-mode 0)))
+(menu-bar-mode -1)
+
+(setq visible-bell t)
+
+(if (member "Hack" (font-family-list))
+    (set-face-attribute 'default nil :font "Hack" :height 120))
+
+(load-theme 'tango-dark)
